27c541634350ed530e5bb27562aa39109111c38c
Lucas de Oliveira Silva <lucasxm21@hotmail.com>
2017-10-14 20:12:43 -0300

    Follow and shoot

--- MyRobot/MyRobot.cs
+++ MyRobot/MyRobot.cs
@@ -7,13 +7,33 @@ using State = System.Collections.Generic.Dictionary<string, object>;
 
 namespace MyRobot {
 	public class MyRobot: IRobot {
-		//s = { player-pos: (x, y), enemy-pos: (x, y) }
-
+		//s = { player-info: (x, y, r), enemy-pos: (x, y) }
+		//p[0] = "action1-action2-action3- ... -actionn", p[1] = {pA1, pA2, pA3 ... pAn}
 		public ActionBlock Update(State s) {
-			double bY = (double) (((ArrayList) s["ball-pos"])[1]);
-			double pY = (double) (((ArrayList) s["player-pos"])[1]);
+			var player_info = (ArrayList) s["player-info"];
+			var enemy_pos = (ArrayList) s["enemy-pos"];
 
-			return new ActionBlock("move", new List<object> { });
+			var actions = "look_to_enemy-move-fire";
+			var ps = new List<object> { 0.0 };
+
+			var distance = Distance(player_info, enemy_pos);
+			if (distance == 15)
+				ps.Add(0.0);
+			else if (distance > 15)
+				ps.Add(100.0);
+			else
+				ps.Add(-100.0);
+
+			ps.Add(20.0 / 3.0 * distance);
+
+			return new ActionBlock("DoAction", new List<object> { actions, ps });
+		}
+
+		public double Distance(ArrayList player_info, ArrayList enemy_pos) {
+			double pX = (double) player_info[0], pY = (double) player_info[1],
+				eX = (double) enemy_pos[0], eY = (double) enemy_pos[1];
+
+			return Math.Sqrt(Math.Pow(pX - eX, 2) + Math.Pow(pY - eY, 2));
 		}
 	}
 }
