a0413eac639720e8ff698304ca9b08cfe3e78821
LucasXM12 <lucasxm21@hotmail.com>
2017-10-20 09:21:06 -0200

    ...

--- MyRobot/MyRobot.cs
+++ MyRobot/MyRobot.cs
@@ -1,5 +1,4 @@
 ﻿using BitsGalaxy;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -8,18 +7,11 @@ using State = System.Collections.Generic.Dictionary<string, object>;
 namespace MyRobot {
 	public class MyRobot: IRobot {
 		//s = { ball-pos: (x, y), player-pos: (x, y) }
-
-		private Random rnd;
-
 		public ActionBlock Update(State s) {
 			double bY = (double) (((ArrayList) s["ball-pos"])[1]);
 			double pY = (double) (((ArrayList) s["player-pos"])[1]);
 
-			return new ActionBlock("move", new List<object> { (double) this.rnd.Next(-5, 5) });
-		}
-
-		public MyRobot() {
-			this.rnd = new Random();
+			return new ActionBlock("move", new List<object> { bY - pY });
 		}
 	}
 }
